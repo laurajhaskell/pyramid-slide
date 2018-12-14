900bb1b5466ecb286d5fb8c45a51a2f8bbd3e46b
Laura Haskell <laurajhaskell@yahoo.com>
2018-12-14 13:39:20 -0600

    base html, css placeholder, range slider function

---
+++ pyramid-slide.js
@@ -0,0 +1,37 @@
+// function show_value(x) {
+//     range = document.getElementById("slider_value");
+//     range.innerHTML = slider.value;
+    
+// };
+
+
+document.getElementById("myRange").oninput = function drawPyramid(myRange) {
+        for (let row = 0; row < myRange; row++) {
+            let numBricks = row + 2;
+            let numSpaces = myRange - row - 1;
+            let rowStr = "";
+            for (let i = 0; i < numSpaces; i++) {
+                rowStr += "."; 
+            }
+            for (let i = 0; i < numBricks; i++) {
+                rowStr += $(this).val();
+            }
+        }
+}
+
+
+// object.oninput = function drawPyramid(height) {
+//     for (let row = 0; row < height; row++) {
+//         let numBricks = row + 2;
+//         let numSpaces = height - row - 1;
+//         let rowStr = "";
+//         for (let i = 0; i < numSpaces; i++) {
+//             rowStr += "."; 
+//         }
+//         for (let i = 0; i < numBricks; i++) {
+//             rowStr += "#";
+//         }
+//     }
+// }
+// object.addEventListener("input", MyScript)
+
