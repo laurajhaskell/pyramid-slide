78ff9b0ce1ecff95e4ee192aa17992f6779c8142
Laura Haskell <laurajhaskell@yahoo.com>
2018-12-14 14:27:53 -0600

    slidePyramid() and drawPyramid()

--- pyramid-slide.js
+++ pyramid-slide.js
@@ -1,53 +1,36 @@
-<<<<<<< HEAD
-// function show_value(x) {
-//     range = document.getElementById("slider_value");
-//     range.innerHTML = slider.value;
-    
-// };
+function slidePyramid() {
+    let slide = document.getElementById("myRange").value;
+    let brick = document.getElementById("brickSymbol").value;
+    document.getElementById("slider_value").innerHTML = slide;
+    //     output.innerHTML = slider.value;
 
-
-document.getElementById("myRange").oninput = function drawPyramid(myRange) {
-        for (let row = 0; row < myRange; row++) {
-            let numBricks = row + 2;
-            let numSpaces = myRange - row - 1;
-            let rowStr = "";
-            for (let i = 0; i < numSpaces; i++) {
-                rowStr += "."; 
-            }
-            for (let i = 0; i < numBricks; i++) {
-                rowStr += $(this).val();
-            }
-        }
+    // slider.oninput = function() {
+    //     output.innerHTML = this.value;
+    // }
+    drawPyramid(slide, brick);
 }
 
 
-// object.oninput = function drawPyramid(height) {
-//     for (let row = 0; row < height; row++) {
-//         let numBricks = row + 2;
-//         let numSpaces = height - row - 1;
-//         let rowStr = "";
-//         for (let i = 0; i < numSpaces; i++) {
-//             rowStr += "."; 
-//         }
-//         for (let i = 0; i < numBricks; i++) {
-//             rowStr += "#";
-//         }
-//     }
-// }
-// object.addEventListener("input", MyScript)
-=======
-object.oninput = function drawPyramid(height) {
-    for (let row = 0; row < height; row++) {
+
+function drawPyramid(myRange, brickSymbol) {
+    document.getElementById("pyramid").innerHTML = "";
+    for (let row = 0; row < myRange; row++) {
         let numBricks = row + 2;
-        let numSpaces = height - row - 1;
+        let numSpaces = myRange - row - 1;
         let rowStr = "";
         for (let i = 0; i < numSpaces; i++) {
             rowStr += "."; 
         }
         for (let i = 0; i < numBricks; i++) {
-            rowStr += "#";
+            rowStr += brickSymbol;
         }
 
-object.addEventListener("input", MyScript)
->>>>>>> aa39890262e87dab54d59890dfa4f003459553d1
+        textElem = document.createTextNode(rowStr);
+
+        rowElem = document.createElement("p");
+        rowElem.appendChild(textElem);
+
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
+}
 
