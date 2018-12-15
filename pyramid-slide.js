b4a2dd4ca54f6810fa51306b32265d80c46d6055
Laura Haskell <laurajhaskell@yahoo.com>
2018-12-14 19:01:11 -0600

    final css styling

--- pyramid-slide.js
+++ pyramid-slide.js
@@ -1,36 +1,35 @@
 function slidePyramid() {
     let slide = document.getElementById("myRange").value;
     let brick = document.getElementById("brickSymbol").value;
-    document.getElementById("slider_value").innerHTML = slide;
-    //     output.innerHTML = slider.value;
-
-    // slider.oninput = function() {
-    //     output.innerHTML = this.value;
-    // }
+    document.getElementById("sliderValue").innerHTML = slide;
     drawPyramid(slide, brick);
 }
 
 
-
 function drawPyramid(myRange, brickSymbol) {
     document.getElementById("pyramid").innerHTML = "";
-    for (let row = 0; row < myRange; row++) {
-        let numBricks = row + 2;
-        let numSpaces = myRange - row - 1;
-        let rowStr = "";
-        for (let i = 0; i < numSpaces; i++) {
-            rowStr += "."; 
+    for (var row = 0; row < myRange; row++) {
+
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = myRange - row - 1;
+
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            let spaceChar = "&nbsp";
+            rowStr += spaceChar;
         }
-        for (let i = 0; i < numBricks; i++) {
+        for (var i = 0; i < numBricks; i++) {
             rowStr += brickSymbol;
         }
 
-        textElem = document.createTextNode(rowStr);
-
+        //create text element with string of chars
         rowElem = document.createElement("p");
-        rowElem.appendChild(textElem);
-
-        document.getElementById("pyramid").appendChild(rowElem);
+        //create <p> element with text inside
+        rowElem.innerHTML = rowStr;
+        //insert paragraph as child of <div> container
+        document.getElementById("pyramid").appendChild(rowElem).style.textAlign = "right";
     }
 }
 
